795564b2631825838acc6f3c811f273a46c4d8fe
microwin7 <usa.microwin8@gmail.com>
2025-04-17 19:54:13 +0300

    Added SensitiveParameter's

--- src/CryptValidator.php
+++ src/CryptValidator.php
@@ -7,20 +7,20 @@ use Hashers\PBKDF2PasswordHasher;
 
 class CryptValidator
 {
-    public static function bcrypt($password, $hash)
+    public static function bcrypt(#[\SensitiveParameter] $password, #[\SensitiveParameter] $hash)
     {
         return password_verify($password, $hash);
     }
-    public static function wp_bcrypt($password, $hash)
+    public static function wp_bcrypt(#[\SensitiveParameter] $password, #[\SensitiveParameter] $hash)
     {
         $password_to_verify = base64_encode( hash_hmac( 'sha384', $password, 'wp-sha384', true ) );
         return password_verify($password_to_verify, substr($hash, 3));
     }
-    public static function phpass($password, $hash)
+    public static function phpass(#[\SensitiveParameter] $password, #[\SensitiveParameter] $hash)
     {
         return PHPass::phpass_validation($password, $hash);
     }
-    public static function pbkdf2($password, $hash)
+    public static function pbkdf2(#[\SensitiveParameter] $password, #[\SensitiveParameter] $hash)
     {
         return (new PBKDF2PasswordHasher())->validate($password, $hash);
     }
